7fa776d43d74fdccc20829ddd3ff46237face398
Rahafsaa <rahaf_saa44@hotmail.com>
2024-08-19 02:38:01 +0300

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
-def players_by_position(squads_list):
-    pass
+def group_by_position(players):
+    position_dict = {}
+    for player in players:
+        position = player['position']
+        if position not in position_dict:
+            position_dict[position] = []
+        position_dict[position].append(player)
+    return position_dict
+
+# Example data
+players_as_dicts = convert_to_dicts(SQUADS_DATA)
+
+# Group by position
+grouped_by_position = group_by_position(players_as_dicts)
+print(grouped_by_position)
