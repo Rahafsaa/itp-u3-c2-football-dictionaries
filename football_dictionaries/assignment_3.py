c5f1a013995fe2812bceede075a66d3de8ded9dc
Rahafsaa <rahaf_saa44@hotmail.com>
2024-08-19 02:39:19 +0300

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,18 @@
-def players_by_country_and_position(squads_list):
-    pass
+def group_by_country_and_position(players):
+    country_dict = {}
+    for player in players:
+        country = player['country']
+        position = player['position']
+        if country not in country_dict:
+            country_dict[country] = {}
+        if position not in country_dict[country]:
+            country_dict[country][position] = []
+        country_dict[country][position].append(player)
+    return country_dict
+
+# Example data
+players_as_dicts = convert_to_dicts(SQUADS_DATA)
+
+# Group by country and position
+grouped_by_country_and_position = group_by_country_and_position(players_as_dicts)
+print(grouped_by_country_and_position)
